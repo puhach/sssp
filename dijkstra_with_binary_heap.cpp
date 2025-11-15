5bcd4dc591c75927ba6d20b8df6d74a908499a2c
puhach <yaroslav.puhach@gmail.com>
2025-11-15 14:52:20 +0200

    Implement Dijkstra search with binary heap

--- dijkstra_with_binary_heap.cpp
+++ dijkstra_with_binary_heap.cpp
@@ -1,6 +1,92 @@
-#include <iostream>
+#include "dijkstra_with_binary_heap.h"
 
-int main()
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <queue>
+
+
+namespace dijkstra_with_binary_heap
 {
-    return 0;
-}
+
+    std::pair<std::vector<int>, std::vector<std::vector<int>>> findShortestPaths(
+        const std::vector<std::vector<Edge>>& graph, 
+        const std::vector<int>& sources,
+        const std::vector<int>& targets)
+    {
+        //constexpr int INF = std::numeric_limits<int>::max();
+        int n = static_cast<int>(graph.size());
+
+        std::vector<int> minDists;
+        std::vector<std::vector<int>> shortestPaths;
+
+        minDists.reserve(sources.size() * targets.size());
+        shortestPaths.reserve(sources.size() * targets.size());
+
+        for (auto source : sources)
+        {
+            std::vector<int> dist(n, infDist);
+            //std::vector<bool> visited(n, false);
+            std::vector<int> prev(n, -1);
+
+            dist[source] = 0;
+
+            using DistToNode = std::pair<int, int>;
+            std::priority_queue<DistToNode, std::vector<DistToNode>, std::greater<DistToNode>> pq;
+            pq.push({0, source});
+
+            while (!pq.empty())
+            {
+                // Extract the node with the shortest distance
+                auto [d, u] = pq.top();
+                pq.pop();
+
+                if (d > dist[u])
+                    continue;
+
+                // Relax all edges from u
+                for (const auto& edge : graph[u])
+                {   
+                    auto v = edge.to;
+                    //auto newDist = d + edge.weight;
+                    auto newDist = dist[u] + edge.weight;
+
+                    if (newDist < dist[v])
+                    {
+                        dist[v] = newDist;
+                        prev[v] = u;
+                        pq.push({newDist, v});
+                    }
+                }   // for edge
+            }   // while
+
+            std::vector<std::vector<int>> paths(n);
+            for (int i = 0; i < n; ++i)
+            {
+                if (dist[i] < infDist)
+                {
+                    //paths[i].reserve(n);
+
+                    for (int cur = i; cur != -1; cur = prev[cur])
+                        paths[i].push_back(cur);
+
+                    std::reverse(paths[i].begin(), paths[i].end());
+                }   // dist[i] < infDist
+            }   // for i
+
+            std::copy(dist.cbegin(), dist.cend(), std::back_inserter(minDists));
+            std::move(paths.begin(), paths.end(), std::back_inserter(shortestPaths));
+        } // for source
+
+        //std::cout << "Shortest distances from node " << start << ":\n";
+        //for (int i = 0; i < n; ++i)
+        //    std::cout << "  to " << i << " = " << dist[i] << "\n";
+
+        //return std::make_pair(std::move(dist), std::move(paths));
+        return std::make_pair(std::move(minDists), std::move(shortestPaths));
+    }   // findShortestPaths
+
+}   // dijkstra_with_binary_heap
+
+
+
---
+++ dijkstra_with_binary_heap.h
@@ -0,0 +1,18 @@
+#ifndef DIJKSTRA_WITH_BINARY_HEAP_H
+#define DIJKSTRA_WITH_BINARY_HEAP_H
+
+#include "edge.h"
+
+#include <tuple>
+#include <vector>
+
+namespace dijkstra_with_binary_heap
+{
+    std::pair<std::vector<int>, std::vector<std::vector<int>>> findShortestPaths(
+        const std::vector<std::vector<Edge>>& graph,
+        const std::vector<int>& sources, 
+        const std::vector<int>& targets);
+
+}   // dijkstra_with_binary_heap
+
+#endif // DIJKSTRA_WITH_BINARY_HEAP_H
